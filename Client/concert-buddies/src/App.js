0c3462efec441b5ccfe39f478eb3f679bf3e08a4
Rachid <74224093+rachelou@users.noreply.github.com>
2023-06-12 13:19:41 +0200

    Upcoming events re (#27)
    * Oud project
    
    Oude project uploaden om te testen met pushen.
    
    * calendar import
    
    calendar import
    
    * Calendarpicker (#26)
    
    * Wiki toegevoegd
    
    * Delete .DS_Store (#11)
    
    * Delete index.js (#12)
    
    * Jeannet's Create profile feature code (#10)
    
    * Jeannet's Create profile feature code (#15)
    
    Co-authored-by: Serenoiia <jeannet.welboren@ziggo.nl>
    
    * Herstructuur mappen (#16)
    
    * Jeannet's Create profile feature code
    
    * mappenstructuur aangepast
    
    ---------
    
    Co-authored-by: Serenoiia <jeannet.welboren@ziggo.nl>
    
    * HTML & CSS aanpassingen (#18)
    
    Co-authored-by: Juul03 <118166691+Juul03@users.noreply.github.com>
    
    * Add artists jh (#19)
    
    * added img folder and artists sub folder
    
    * controller toegevoegd
    
    * styling toegevoegd
    
    * Toevoegen HTML my-events
    
    * Delete m_events.ejs
    
    Verkeerde file gepushed
    
    * Toevoegen correcte html my-events pagina
    
    * toevoegen correcte HTML voor my-events pagina
    
    * toevoegen HTML event-details
    
    * Delete .enf (#22)
    
    * Delete .enf (#23)
    
    * delete ds store (#24)
    
    * Dev (#25)
    
    * Jeannet's Create profile feature code
    
    * mappenstructuur aangepast
    
    * updated gitignore to ignore DS_store
    
    ---------
    
    Co-authored-by: Serenoiia <jeannet.welboren@ziggo.nl>
    
    ---------
    
    Co-authored-by: Matthew-Tangk <matthew.tangkilissan@gmail.com>
    Co-authored-by: Juul03 <118166691+Juul03@users.noreply.github.com>
    Co-authored-by: Jeannet Welboren <118215949+Serenoiia@users.noreply.github.com>
    Co-authored-by: Serenoiia <jeannet.welboren@ziggo.nl>
    Co-authored-by: Matthew Tangkilissan <118222001+Matthew-Tangk@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Matthew-Tangk <matthew.tangkilissan@gmail.com>
    Co-authored-by: Juul03 <118166691+Juul03@users.noreply.github.com>
    Co-authored-by: Jeannet Welboren <118215949+Serenoiia@users.noreply.github.com>
    Co-authored-by: Serenoiia <jeannet.welboren@ziggo.nl>
    Co-authored-by: Matthew Tangkilissan <118222001+Matthew-Tangk@users.noreply.github.com>

---
+++ Client/concert-buddies/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import CustomMonthLayout from "./components/DateCalendarPicker.js";
+
+const calendar = () => {
+  return (
+    <div>
+      {/* Other components or JSX */}
+      <CustomMonthLayout />
+      {/* Other components or JSX */}
+    </div>
+  );
+};
+
+export default calendar;
