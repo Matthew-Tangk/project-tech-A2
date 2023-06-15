e8c17c287d19d2c3280b58b310d9286f27652f25
Serenoiia <jeannet.welboren@ziggo.nl>
2023-06-15 10:55:54 +0200

    Back-end Registratie & homepage - jw

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,10 +7,10 @@
     if (file) {
       const reader = new FileReader();
 
-      reader.addEventListener("error", (event) => {
-        const error = reader.error;
-        errorMessage.textContent = `Error reading the file ${error.message}`;
-      });
+      // reader.addEventListener("error", (event) => {
+      //   const error = reader.error;
+      //   errorMessage.textContent = `Error reading the file ${error.message}`;
+      // });
 
       reader.readAsDataURL(file);
       document.querySelector("img").src = URL.createObjectURL(file);
@@ -19,25 +19,25 @@
   });
 
   /* Image upload server-side check */
-  document.querySelector("#upload-image").addEventListener("change", (event) => {
-    const messageElement = document.querySelector("#message");
-    const file = event.target.files[0];
+  // document.querySelector("#upload-image").addEventListener("change", (event) => {
+  //   const messageElement = document.querySelector("#message");
+  //   const file = event.target.files[0];
     
-    if (file) {
-      const fr = new FileReader();
-      fr.onload = () => {
-        if (fr.readyState === FileReader.DONE) {
-          messageElement.textContent = "File uploaded successfully!";
-          messageElement.style.color = "green";
-        }
-      };
-      fr.onerror = () => {
-        messageElement.textContent = "There was an error with uploading the file.";
-        messageElement.style.color = "red";
-      };
-      fr.readAsDataURL(file);
-    }
-  });
+  //   if (file) {
+  //     const fr = new FileReader();
+  //     fr.onload = () => {
+  //       if (fr.readyState === FileReader.DONE) {
+  //         messageElement.textContent = "File uploaded successfully!";
+  //         messageElement.style.color = "green";
+  //       }
+  //     };
+  //     fr.onerror = () => {
+  //       messageElement.textContent = "There was an error with uploading the file.";
+  //       messageElement.style.color = "red";
+  //     };
+  //     fr.readAsDataURL(file);
+  //   }
+  // });
 
   /* Spotify embed */
   window.onSpotifyIframeApiReady = (IFrameAPI) => {
