ed2fd1e499d8da8a5cf900992304515e83d86671
Juul03 <118166691+Juul03@users.noreply.github.com>
2023-06-19 15:28:52 +0200

    profile post request gefixt

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -23,9 +23,34 @@ async function run() {
 
 run().catch(console.dir);
 
+const dbName = "usersData";
+const collectionName = "user";
+
 const tinify = require("tinify");
-tinify.key = "9v2Bl9fzC6qgqXWvTJ0sCSt19L33nt2M";
+tinify.key = process.env.KEY;
 tinify.fromFile("assets/static/img/artists/arcticmonkeys.jpg").toFile("assets/static/img/artists/optimized/arcticoptimized.png");
+
+// TINIFY alle afbeeldingen in 1 keer
+const fs = require('fs');
+
+const artistImgPath = "assets/static/img/artists";
+
+let filesArray = fs.readdir(artistImgPath, (err, files) => {
+  if(err) {
+    console.log('Error reading folder', err);
+    return;
+  }
+
+  const artistImagesFiles = files.filter(file => {
+    const extension = file.split('.').pop().toLowerCase();
+    return ['jpg', 'png'].includes(extension);
+  })
+
+  artistImagesFiles.forEach(file => { 
+    tinify.fromFile("assets/static/img/artists/" + file).toFile("assets/static/img/artists/optimized/" + file);
+  });
+  })
+
 
 exports.addArtists = async (req, res) => {
   try {
@@ -55,17 +80,56 @@ exports.profile = async (req, res) => {
     const favoriteArtists = {selectedFavoriteArtists}
     
     try {
-      await sendFavoriteArtistData(favoriteArtists);
-
-      // Hier de profielData nog toevoegen door die uit de database te halen
+      await client.connect();
+  
+      const profileDataCollection = client.db(dbName).collection(collectionName)
+      const userData = await profileDataCollection.findOne({}, { sort: { _id: -1}});
+  
+      if (userData) {
+        const profileData = {
+          username: userData.username,
+          age: userData.age,
+          file: userData.file,
+          about: userData.about,
+        };
+  
+      // Retrieve favorite genres from db
+      const selectedGenreCollection = client.db('concertBuddies').collection('selectedGenres')
+      const allSelectedGenreData = await selectedGenreCollection.find({}).toArray();
+  
+      // Retrieve favorite artists from db
+      const favoriteArtists = client.db('concertBuddies').collection('favoriteArtists');
+      const mostRecentFavArtists = await favoriteArtists.findOne({}, { sort: { _id: -1 } });
+      // Write a statement to prevent error when favArtistData is empty
+      const favArtistsData = mostRecentFavArtists ? mostRecentFavArtists.selectedFavoriteArtists : [];
       
-      res.render('profile.ejs', {
-        title:"My profile",
-        profileData: profileData
-      })
-    } catch(err) {
-      console.error("Something went wrong with sending data to the db", err);
+      const foundObjectsFromFavoriteArtists = [];
+  
+      if (favArtistsData && favArtistsData.length > 0) {
+        const retrieveAdditionalArtistData = favArtistsData.map(artist => {
+          const findAllInfoOfFavArtist = async() => { 
+            try {
+              await client.connect();
+              const allArtist = client.db('concertBuddies').collection('artists');
+              let nowActiveArtists = await allArtist.find({ name: artist }).toArray();
+              foundObjectsFromFavoriteArtists.push(...nowActiveArtists);
+            } catch (error) {
+              console.error("An error occurred while retrieving the data from the db", error);
+            } 
+          };
+          return findAllInfoOfFavArtist();
+        });
+        await Promise.all(retrieveAdditionalArtistData);
+      }
+      
+      res.render("profile", { profileData: profileData, title: "My profile", selectedGenre: allSelectedGenreData, favoriteArtists: favArtistsData, additionFavoriteArtistsData:foundObjectsFromFavoriteArtists });
+    } else {
+      res.render("profile", { profileData: null, title: "My profile", selectedGenre: allSelectedGenreData, favoriteArtists: mostRecentFavArtists  });
     }
+    } catch (error) {
+      console.error("An error occurred while saving the data:", error);
+      res.render("error.ejs");
+    } 
 }
 
 const sendFavoriteArtistData = async (data) => {
@@ -81,3 +145,22 @@ const sendFavoriteArtistData = async (data) => {
   }
 }
 
+// WERKT NIET
+// const updateFavoriteArtists = async (data) => {
+//   try {
+//     const favoriteArtists = client
+//     .db("userData")
+//     .collection("user");
+
+//     const uploadFavoriteArtistsData =
+//     await favoriteArtists.findOneAndUpdate(
+//       { "username" : "testjulia" },
+//       { $set: { "favartists" : data } })
+
+//       console.log("get data" + uploadFavoriteArtistsData);
+
+//       console.log("The artists are succesfully added to profile", uploadFavoriteArtistsData.insertedId);
+//   } catch(err) {
+//     console.error("Something went wrong with adding the artists to your profile :(", err);
+//   }
+// }
