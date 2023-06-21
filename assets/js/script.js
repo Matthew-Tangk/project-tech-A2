476b6eb7691f0a752beaa34c4a7bca2b78624418
Juul03 <118166691+Juul03@users.noreply.github.com>
2023-06-21 15:08:08 +0200

    update script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,45 @@
+ /* Image upload preview */
+ const fileInput = document.querySelector("#upload-image");
+ const errorMessage = document.querySelector("#error-message");
+
+ fileInput.addEventListener("change", () => {
+   const file = fileInput.files[0];
+   if (file) {
+     const reader = new FileReader();
+
+     reader.addEventListener("error", (event) => {
+       const error = reader.error;
+       errorMessage.textContent = `Error reading the file ${error.message}`;
+     });
+
+     reader.readAsDataURL(file);
+     document.querySelector("img").src = URL.createObjectURL(file);
+     
+   }
+ });
+
+ /* Image upload server-side check */
+ document.querySelector("#upload-image").addEventListener("change", (event) => {
+   const messageElement = document.querySelector("#message");
+   const file = event.target.files[0];
+   
+   if (file) {
+     const fr = new FileReader();
+     fr.onload = () => {
+       if (fr.readyState === FileReader.DONE) {
+         messageElement.textContent = "File uploaded successfully!";
+         messageElement.style.color = "green";
+       }
+     };
+     fr.onerror = () => {
+       messageElement.textContent = "There was an error with uploading the file.";
+       messageElement.style.color = "red";
+     };
+     fr.readAsDataURL(file);
+   }
+ });
+
+ 
 const showNotificationInvite = (notificationMessage) => {
   const notification = document.createElement('div');
   notification.classList.add('notification');
